78935ea2d4e686074beabfb1b972fd7bc995237c
Hasindu Udara <hasindutechschool@gmail.com>
2025-08-04 11:15:19 +0530

    update

--- src/main/java/lk/ijse/gdse/springbootdeployment/entity/Customer.java
+++ src/main/java/lk/ijse/gdse/springbootdeployment/entity/Customer.java
@@ -9,7 +9,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-@Table(name = "customers")
 public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
